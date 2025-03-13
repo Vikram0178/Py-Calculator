a426c2663abdcdf7a1b1116dcf89852cf903dffb
Vikram0178 <128591267+Vikram0178@users.noreply.github.com>
2025-03-13 23:02:37 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,7 @@
+print("Hello,My friend Thanks for using this script im making for building my portfolio")
+print("This is py Calculator You have to enter two numbers when asked and select the operation to do and all set")
+n1 = input("please enter your 1st number: ")
+n2 = input("please enter your 1st number: ")
+
+o = ["add" , "multiply" ]
+input 
